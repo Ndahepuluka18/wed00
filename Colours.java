c1093189805597e0e48411e14d0c7520aab9ec21
Ndahepuluka18 <jennifernghipunya@gmail.com>
2024-10-15 13:01:42 +0200

    Add files via upload

---
+++ Colours.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import java.awt.*;
+
+public class Colours {
+    public static final Color PRIMARY_COLOR = Color.decode("#191E29");
+    public static final Color SECONDARY_COLOR = Color.decode("#132D46");
+    public static final Color TEXT_COLOR = Color.decode("#01C380");
+}
